8b942466627e7f0ba88ecf0772d7af4317ba71d9
Dmitry Buryak <boss23rus@gmail.com>
2017-05-25 22:02:16 +0300

    fix notification message

--- frontend/src/components/DesktopNotification.js
+++ frontend/src/components/DesktopNotification.js
@@ -3,11 +3,12 @@ import Notification from 'react-web-notification/lib/components/Notification';
 
 const DesktopNotification = ({message, title}) => {
     if (message && !message.isOwn) {
-        let body = `[${message.username}] ${message.text}`;
+        let body = message.username ? `[${message.username}] ${message.text}` : `${message.text}`;
         return (
             <Notification title={title} options={{body: body}}/>
         );
     }
+
     return (
         <Notification title={title} ignore={true}/>
     );
